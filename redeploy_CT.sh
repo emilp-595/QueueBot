698908e9db95a4fb2c8cd1bdfe7ec824f8ade7c9
thomasgonda3 <thomasgonda3@gmail.com>
2024-08-10 13:37:28 -0700

    fixed redeploy files for linux

--- redeploy_CT.sh
+++ redeploy_CT.sh
@@ -4,4 +4,4 @@ docker rm ct-mkwii-mogi-queuebot
 docker container prune
 docker build --tag ct-mkwii-mogi-queuebot .
 docker image prune
-docker run -d --name ct-mkwii-mogi-queuebot -v ${pwd}\settings_data:/app/settings_data --restart unless-stopped ct-mkwii-mogi-queuebot
+docker run -d --name ct-mkwii-mogi-queuebot -v $(pwd)/settings_data:/app/settings_data --restart unless-stopped ct-mkwii-mogi-queuebot
--- redeploy_MK8DX.sh
+++ redeploy_MK8DX.sh
@@ -4,4 +4,4 @@ docker rm mogi-queuebot
 docker container prune
 docker build --tag mogi-queuebot .
 docker image prune
-docker run -d --name mogi-queuebot -v ${pwd}\settings_data:/app/settings_data --restart unless-stopped mogi-queuebot
+docker run -d --name mogi-queuebot -v $(pwd)/settings_data:/app/settings_data --restart unless-stopped mogi-queuebot
